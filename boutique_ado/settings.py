3ddae9da9041eb94d9ecc28e81355582061048a1
Martina Martin <marti.law@gmail.com>
2022-09-12 08:35:01 +0000

    Complete confirmation emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,6 +180,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_KEY = os.getenv('STRIPE_WH_KEY', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 
 # Default primary key field type
